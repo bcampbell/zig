029f7088dc3fc0a928c8c3a1c035a914caa07a2b
Ben Campbell <ben@scumways.com>
2014-11-18 21:04:15 +1300

    fix up stk zero attack/decay warnings

--- src/retromat/retromat.cpp
+++ src/retromat/retromat.cpp
@@ -19,7 +19,7 @@ void GenerateLaser( std::vector<float>& out )
 		0.2f,						// duration
 		SINE, 440.0f, 10.0f,		// src: type, startf, endf
 		SQUARE, 0.0f, 0.0f, 0.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.01f, 0.7f, 0.1f,	// adsr
+		0.0001f, 0.01f, 0.7f, 0.1f,	// adsr
 		0.0f, 0.95f,				// filter: startpole, endpole
 		0.3f						// gain
 	};
@@ -37,7 +37,7 @@ void GenerateBaiterAlert( std::vector<float>& out )
 		1.0f,						// duration, gain
 		TRIANGLE, 440.0f, 2500.0f,		// src: type, startf, endf
 		SINE, 5.0f, 100.0f, 1000.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.00f, 1.0f, 0.1f,	// adsr
+		0.0001f, 0.0001f, 1.0f, 0.1f,	// adsr
 		0.95f, -0.95f,				// filter: startpole, endpole
 		0.3f						// gain
 	};
@@ -56,7 +56,7 @@ void GeneratePlayerToast( std::vector<float>& out )
 		1.0f,						// duration, gain
 		SQUARE, 1.0f, 50.0f,		// src: type, startf, endf
 		SINE, 500.0f, 1.0f, 1000.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.00f, 1.0f, 0.1f,	// adsr
+		0.0001f, 0.0001f, 1.0f, 0.1f,	// adsr
 		-0.9f, 0.9f,				// filter: startpole, endpole
 		0.3f						// gain
 	};
@@ -73,7 +73,7 @@ void GenerateLevelIntro( std::vector<float>& out )
 		2.0f,						// duration, gain
 		SQUARE, 8000.0f, -1000.0f,		// src: type, startf, endf
 		SAWTOOTH, 2000.0f, 1.0f, 1000.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.2f, 0.7f, 1.0f,	// adsr
+		0.0001f, 0.2f, 0.7f, 1.0f,	// adsr
 		0.9f, 0.9f,				// filter: startpole, endpole
 		1.0f						// gain
 	};
@@ -90,7 +90,7 @@ void GenerateBigExplosion( std::vector<float>& out )
 		2.0f,						// duration, gain
 		SQUARE, 100.0f, 10.0f,		// src: type, startf, endf
 		SAWTOOTH, 12000.0f, 10.0f, 1000.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.2f, 0.7f, 1.0f,	// adsr
+		0.0001f, 0.2f, 0.7f, 1.0f,	// adsr
 		0.9f, 0.9f,				// filter: startpole, endpole
 		1.0f						// gain
 	};
@@ -107,7 +107,7 @@ void GenerateGameOver( std::vector<float>& out )
 		3.0f,						// duration, gain
 		SQUARE, 80.0f, 1.0f,		// src: type, startf, endf
 		SQUARE, 5.0f, 0.0f, 20.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.2f, 0.7f, 0.1f,	// adsr
+		0.0001f, 0.2f, 0.7f, 0.1f,	// adsr
 		0.9f, -0.9f,				// filter: startpole, endpole
 		1.0f						// gain
 	};
@@ -127,7 +127,7 @@ void GenerateDullBlast( std::vector<float>& out )
 		0.15f,						// duration
 		SQUARE, 200.0f, 1.0f,			// src: type, startf, endf
 		SINE, 	10000.0f, 10.0f, 10.0f,	// mod: type, startf, endf, ampl
-		0.0f, 0.01f, 0.7f, 0.05f,	// adsr
+		0.0001f, 0.01f, 0.7f, 0.05f,	// adsr
 		0.95f, 0.95f,				// filter: startpole, endpole
 		0.3f						// gain
 	};
