95ddfef456e63f818cc1850e8173bb84a010a77c
Ben Campbell <ben@scumways.com>
2014-11-25 13:21:19 +1300

    track libpng change made sometime earlier in the decade...

--- src/loadpng.cpp
+++ src/loadpng.cpp
@@ -83,7 +83,7 @@ static SDL_Surface* Read( FILE* fp )
 			png_set_palette_to_rgb( png_ptr );
 
 		if( colourtype == PNG_COLOR_TYPE_GRAY && bitdepth<8 )
-			png_set_gray_1_2_4_to_8( png_ptr );
+			png_set_expand_gray_1_2_4_to_8( png_ptr );
 
 	//    if( png_get_valid(png_ptr, info_ptr,
 	//        PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
