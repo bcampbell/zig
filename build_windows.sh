3a16066719644dc3ac3aaaefd96b6a4739d3f6d1
Ben Campbell <ben@scumways.com>
2018-01-29 22:49:10 +1300

    windows build fixes and notes

--- build_windows.sh
+++ build_windows.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 set -e
 
-dlls="libpng16-16.dll SDL2.dll SDL2_mixer.dll libwinpthread-1.dll"
+# windows build checklist:
+#
+# 32-bit build?
+# runs from explorer?
+# cheats turned off?
+# binaries stripped?
+# soak test (with and without -flatout)
+# check sounds sound right
+
+
+#dlls="libpng16-16.dll SDL2.dll SDL2_mixer.dll libwinpthread-1.dll"
 
 
 dest=`mktemp -d`
@@ -9,12 +19,13 @@ echo $dest
 
 pushd src >/dev/null
 make clean
-make -j4
+make -j4 release
+strip zig.exe
 cp zig.exe $dest/
 cp -r data $dest/
 popd >/dev/null
 
-cp `which $dlls` $dest/
+#cp `which $dlls` $dest/
 
 
 echo $dest
