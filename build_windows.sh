56b20bef1cfebabb543acfe92a2c977eb32ca342
Ben Campbell <ben@scumways.com>
2018-02-28 22:53:51 +1300

    more windows build tweakage

--- build_windows.sh
+++ build_windows.sh
@@ -14,7 +14,21 @@ set -e
 #dlls="libpng16-16.dll SDL2.dll SDL2_mixer.dll libwinpthread-1.dll"
 
 
-dest=`mktemp -d`
+#for our custom sdl_mixer build
+SITE=~/site32
+export PKG_CONFIG_PATH=$SITE/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# BUILD zig
+
+t=`mktemp -d`
+dest=$t/Zig
+mkdir $dest
+echo $dest
+
+
+t=`mktemp -d`
+dest=$t/Zig
+mkdir $dest
 echo $dest
 
 pushd src >/dev/null
@@ -28,7 +42,9 @@ popd >/dev/null
 #cp `which $dlls` $dest/
 
 
-echo $dest
-
-
+# zip it
+cd $t
+z=zig_windows_$(date +%Y-%d-%m).zip
+zip -r $z $(basename $dest)
+echo done
 
